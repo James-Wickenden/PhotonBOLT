e7ee34ded3d1683139059a83ff7515b253163b94
Adrian David <dreiclark@gmail.com>
2019-11-14 00:05:31 +0000

    lan connection functional

--- Assets/NetworkCallbacks.cs
+++ Assets/NetworkCallbacks.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[BoltGlobalBehaviour]
 public class NetworkCallbacks : Bolt.GlobalEventListener
 {
     public override void SceneLoadLocalDone(string scene) {
-        var spawnPosition = new Vector3(Random.range(-8,8),0,Random.range(-8,8));
+        var spawnPosition = new Vector3(Random.Range(-8,8),0,Random.Range(-8,8));
         BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
 
     }
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : Bolt.EntityBehaviour<ICustomCubeState>
+{
+    public override void Attached(){
+        state.SetTransforms(state.CubeTransform, gameObject.transform);
+    }
+
+    public override void SimulateOwner(){
+        var moveDirection = Vector3.zero;
+        var movementSpeed = 5f;
+        
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        
+        if (moveDirection != Vector3.zero) {
+            transform.position = transform.position + (moveDirection.normalized * movementSpeed * BoltNetwork.FrameDeltaTime);
+        }
+    }
+}
