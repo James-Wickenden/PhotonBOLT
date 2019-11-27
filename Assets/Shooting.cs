79bb52618b9611e13e5509dff4977f37f7a77d65
James <james@hawkinsplace.co.uk>
2019-11-27 12:55:57 +0000

    added some boolets :)

---
+++ Assets/Shooting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public float bulletSpeed;
+    public GameObject muzzle;
+
+    private void Shoot()
+    {
+
+        Rigidbody bulletClone = Instantiate(bulletPrefab, muzzle.transform.position, this.transform.rotation);
+
+        bulletClone.velocity = transform.TransformDirection(new Vector3(0,0, bulletSpeed));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+}
