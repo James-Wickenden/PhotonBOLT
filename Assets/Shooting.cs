f81e65e66744442977fa9b383248800f9bf47669
James Wickenden <jameswickenden14@gmail.com>
2019-12-18 16:02:52 +0000

    added health

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -10,14 +10,14 @@ public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
 
     public override void Attached() {
         state.OnShoot = Shoot;
-
+        state.Health = 100.0F;
     }
 
     private void Shoot()
     {
 
         Rigidbody bulletClone = Instantiate(bulletPrefab, muzzle.transform.position, this.transform.rotation);
-
+        if (entity.IsOwner) state.Health -= 1F;
         bulletClone.velocity = transform.TransformDirection(new Vector3(0,0, bulletSpeed));
     }
 
@@ -26,6 +26,7 @@ public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
         if (Input.GetKeyDown(KeyCode.Space) && entity.IsOwner)
         {
             state.Shoot();
+            Debug.Log("health is " + state.Health.ToString() + "from tank " + this.GetInstanceID());
         }
     }
 }
