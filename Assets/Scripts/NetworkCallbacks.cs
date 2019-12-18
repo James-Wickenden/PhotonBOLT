cf194454e89bc924a8a3e24136dab6ad42b03278
James <james@hawkinsplace.co.uk>
2019-12-18 17:04:56 +0000

    replaced cube with basic tank prefab

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -8,7 +8,7 @@ public class NetworkCallbacks : Bolt.GlobalEventListener
     public override void SceneLoadLocalDone(string scene) {
 
         var spawnPosition = new Vector3(Random.Range(-8,8),1,Random.Range(-8,8));
-        BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+        BoltNetwork.Instantiate(BoltPrefabs.tank, spawnPosition, Quaternion.identity);
         
     }
 }
