5a95a81feaa9634e5c3a498ceb91a932764d9088
ainesh1998 <ainesh1998@outlook.com>
2019-12-19 10:34:17 +0000

    turret now rotates with camera

---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ensure turret's y-rotation follows the camera's
+        var CharacterRotation = Camera.main.transform.rotation;
+        CharacterRotation.x = 0;
+        CharacterRotation.z = 0;
+
+        transform.rotation = CharacterRotation;
+    }
+}
