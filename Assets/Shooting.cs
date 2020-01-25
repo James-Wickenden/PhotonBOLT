236f9b1bb9751bf37306d94c4bda9ef226ef81af
James Wickenden <jameswickenden14@gmail.com>
2020-01-25 17:01:16 +0000

    collision detection hell

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
 {
+    public HashSet<Rigidbody> bullets = new HashSet<Rigidbody>();
     public Rigidbody bulletPrefab;
     public float bulletSpeed;
     public GameObject muzzle;
@@ -20,6 +21,7 @@ public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
     private void Shoot()
     {
         Rigidbody bulletClone = Instantiate(bulletPrefab, muzzle.transform.position, this.transform.rotation);
+        bullets.Add(bulletClone);
         if (entity.IsOwner) state.Health -= 1F;
         bulletClone.velocity = transform.TransformDirection(new Vector3(0,0, bulletSpeed));
     }
@@ -29,7 +31,9 @@ public class Shooting : Bolt.EntityBehaviour<ICustomCubeState>
         if (Input.GetKeyDown(KeyCode.Space) && entity.IsOwner)
         {
             state.Shoot();
-            Debug.Log("health is " + state.Health.ToString() + "from tank " + this.GetInstanceID());
+            Debug.Log("health is " + state.Health.ToString() + " from tank " + this.GetInstanceID());
+            Debug.Log("bullets contains " + this.bullets.Count + " from tank " + this.GetInstanceID());
         }
+
     }
 }
