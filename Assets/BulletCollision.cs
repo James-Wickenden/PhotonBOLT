ad69a84c42b1ab93a314dfbb57bf3b87f5b4483c
James <james@hawkinsplace.co.uk>
2020-01-25 17:31:15 +0000

    bullet collision script added

---
+++ Assets/BulletCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit!");
+        // TODO: destory if touching other tank.
+        // Only 
+
+        //Destroy(gameObject);
+    }
+}
