4df2cde01541586c49a42b8b9e83919a5207eea0
James <james@hawkinsplace.co.uk>
2020-01-25 17:00:58 +0000

    updated menu

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+    public event System.Action<float> OnHealthPctChanged = delegate { };
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ModifyHealth(int amount)
+    {
+        currentHealth += amount;
+
+        float currentHealthPct = (float) currentHealth / (float) maxHealth
+        OnHealthPctChanged(currentHealthPct);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.H))
+        {
+            ModifyHealth(-10);
+        }
+    }
+}
