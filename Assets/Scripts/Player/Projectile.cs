cceceaa86d06536dd2dc6092c7b7da9c8a997984
James <james@hawkinsplace.co.uk>
2020-02-02 17:25:49 +0000

    tested and resized healthbar

---
+++ Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    public float damage;
+
+    [SerializeField]
+    public float speed;
+
+    private int sourceID;
+
+    public void setSourceID(int sourceID)
+    {
+        this.sourceID = sourceID;
+    }
+
+    public int getSourceID()
+    {
+        return sourceID;
+    }
+
+    public float getSpeed()
+    {
+        return speed;
+    }
+
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
