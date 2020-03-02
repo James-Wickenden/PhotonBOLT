aa77e8d12a6b61a6313731372b6af95d978cd797
James Wickenden <jameswickenden14@gmail.com>
2020-03-02 17:04:16 +0000

    initial scoreboard toggle setup/ instantiation

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+    private bool isScoreboardOpen = false;
+
+    private void Start() {
+        Button scoreboardButton = GameObject.FindGameObjectWithTag("ScoreboardButton").GetComponent<Button>();
+        scoreboardButton.onClick.AddListener(() => ToggleScoreboard());
+        Debug.Log("Initialised scoreboard listener");
+    }
+
+    private void ToggleScoreboard() {
+        Debug.Log("Detected scoreboard toggle");
+        if (!isScoreboardOpen) {
+            isScoreboardOpen = true;
+            Debug.Log("Opened scoreboard");
+        }
+        else {
+            isScoreboardOpen = false;
+            Debug.Log("Closed scoreboard");
+        }
+    }
+
+    private void Update() {
+        
+    }
+}
