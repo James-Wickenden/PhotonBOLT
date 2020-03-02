6da86be0714775eca57cf5542146ecb441789b9c
James Wickenden <jameswickenden14@gmail.com>
2020-03-02 18:42:58 +0000

    added the UI elements; TODO dynamically create scoreboard for a clean scene and import/ display scores

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -2,30 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon;
 
-public class Scoreboard : MonoBehaviour
+public class Scoreboard : Bolt.GlobalEventListener
 {
     private bool isScoreboardOpen = false;
+    public GameObject scoreboard;
+    private Dictionary<string, string> scoreMap = new Dictionary<string, string>();
 
     private void Start() {
         Button scoreboardButton = GameObject.FindGameObjectWithTag("ScoreboardButton").GetComponent<Button>();
         scoreboardButton.onClick.AddListener(() => ToggleScoreboard());
+        scoreboard.SetActive(false);
         Debug.Log("Initialised scoreboard listener");
     }
 
     private void ToggleScoreboard() {
-        Debug.Log("Detected scoreboard toggle");
         if (!isScoreboardOpen) {
             isScoreboardOpen = true;
-            Debug.Log("Opened scoreboard");
+            refreshScoreboard();
+            scoreboard.SetActive(true);
+            Debug.Log("Opened scoreboard; " + scoreMap.Count + " players");
+
         }
         else {
             isScoreboardOpen = false;
+            scoreboard.SetActive(false);
             Debug.Log("Closed scoreboard");
+
         }
     }
 
-    private void Update() {
-        
+    private void refreshScoreboard() {
+        scoreMap.Clear();
+
+        string playerID, playerScore;
+        foreach (var player in BoltNetwork.Connections)
+        {
+            playerID = player.UserData.ToString();
+            playerScore = GetInstanceID().ToString();
+            scoreMap.Add(playerID, playerScore);
+        }
+        scoreMap.Add("1", "100");
+        scoreMap.Add("2", "200");
+        scoreMap.Add("3", "300");
     }
 }
