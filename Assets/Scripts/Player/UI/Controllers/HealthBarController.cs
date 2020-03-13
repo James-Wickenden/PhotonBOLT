fcba4f0159c039cb0811977b6cd7ca458c857b0d
James Wickenden <jameswickenden14@gmail.com>
2020-03-13 18:36:28 +0000

    WIP Merge FML

---
+++ Assets/Scripts/Player/UI/Controllers/HealthBarController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarController : MonoBehaviour
+{
+    [SerializeField]
+    private HealthBar healthBarPrefab;
+
+    private Dictionary<Health, HealthBar> healthBars = new Dictionary<Health, HealthBar>();
+
+    private void Awake()
+    {
+        Health.OnHealthAdded += AddHealthBar;
+        Health.OnHealthRemoved += RemoveHealthBar;
+    }
+
+    private void AddHealthBar(Health health)
+    {
+        if (!healthBars.ContainsKey(health))
+        {
+            var healthBar = Instantiate(healthBarPrefab, transform);
+            healthBar.SetHealth(health);
+            healthBars.Add(health, healthBar);
+        }
+    }
+
+    private void RemoveHealthBar(Health health)
+    {
+        if (healthBars.ContainsKey(health))
+        {
+            Destroy(healthBars[health].gameObject);
+            healthBars.Remove(health);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/UI/Controllers/HitmarkerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitmarkerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject hitmarkerPrefab;
+
+    GameObject spawned;
+
+    float endTime;
+
+    [SerializeField]
+    float hitmarkerDuration = 0.025f;
+
+    void Awake()
+    {
+        HitDetection.OnPlayerHitByMe += StartHitmarkerAnimation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawned != null && Time.time > endTime)
+        {
+            Destroy(spawned);
+        }
+    }
+
+    void StartHitmarkerAnimation()
+    {
+        spawned = Instantiate(hitmarkerPrefab, transform);
+        endTime = Time.time + hitmarkerDuration;
+    }
+
+}
